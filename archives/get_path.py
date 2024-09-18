cbd9a0f303ec60095a41cbff0d82714453585a5e
sylvainrocheleau <info@sylvainrocheleau.com>
2024-09-18 10:24:17 -0400

    first commit

--- archives/get_path.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-# get current directory
-cookie = [{}]
-parent = os.path.dirname(os.getcwd())
-with open(parent+"/scrapy_playwright_ato/spiders/cookies.json", "w") as f:
-    f.write(json.dumps(cookie))
